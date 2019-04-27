2625299aae79c28cd804a7b571306f677fd9ece0
Emircan Küçükmotor <emircan.kucukmotor92@gmail.com>
2019-04-27 22:17:15 +0300

    Added a User Model for future registration/login implementations.

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from wtforms import StringField, PasswordField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
 
 from datetime import datetime
 
@@ -31,6 +33,47 @@ class Blogpost(db.Model):
     author = db.Column(db.String(20))
     date_posted = db.Column(db.DateTime)
     content = db.Column(db.Text)
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password_plaintext = db.Column(db.String, nullable=False)  # TEMPORARY - TO BE DELETED IN FAVOR OF HASHED PASSWORD
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def __init__(self, username, password_plaintext):
+        self.username = username
+        self.password_plaintext = password_plaintext
+        self.authenticated = False
+
+    @hybrid_method
+    def is_correct_password(self, plaintext_password):
+        return self.password_plaintext == plaintext_password
+
+    @property
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    @property
+    def is_active(self):
+        """Always True, as all users are active."""
+        return True
+
+    @property
+    def is_anonymous(self):
+        """Always False, as anonymous users aren't supported."""
+        return False
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        """Requires use of Python 3"""
+        return str(self.id)
+
+    def __repr__(self):
+        return '<User {0}>'.format(self.name)
 
 
 class BlogpostForm(Form):
